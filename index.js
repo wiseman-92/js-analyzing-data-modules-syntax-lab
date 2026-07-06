37389d8196836c88f4f275b6ddd601b997958deb
Branton Kipruto Birgen <brantonbirgen@gmail.com>
2026-07-06 22:08:10 +0300

    edit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-
+require ('datejs')
+function combineUsers (...args) {
+  const combinedObject = {
+    users: []
+  }
+  args.forEach(item => combinedObject.users.push(...item))
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy')
+  return combinedObject
+}
 
 
 module.exports = {
